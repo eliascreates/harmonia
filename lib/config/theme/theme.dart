de85d6f6e81e3b7748e59cbb36b78ef8bb51f98e
Elias Kekana <thapeloelias.kekana@gmail.com>
2023-10-20 23:55:30 +0200

    style: add themes for dark mode and light mode

--- lib/config/theme/theme.dart
+++ lib/config/theme/theme.dart
@@ -1,1 +1,52 @@
 //Dark Mode canvas color = ff201919
+
+import 'package:flutter/material.dart';
+
+const darkBackgroundColor = Color(0xff201919);
+const primaryColor = Color(0xffbf564c);
+const accentColor = Color(0xffa13e38);
+const overlayColor = Color(0xffe5675b);
+
+final appLightTheme = ThemeData(
+  fontFamily: 'Poppins',
+  colorScheme: ColorScheme.fromSeed(
+    brightness: Brightness.light,
+    primary: primaryColor,
+    seedColor: primaryColor,
+  ),
+  textTheme: const TextTheme(
+    headlineMedium: TextStyle(color: Color(0xffa13e38)),
+    titleLarge: TextStyle(color: Color(0xff775651)),
+  ),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ButtonStyle(
+      shape: MaterialStatePropertyAll(
+        RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+      ),
+      foregroundColor: const MaterialStatePropertyAll(Colors.white),
+      backgroundColor: const MaterialStatePropertyAll(primaryColor),
+      overlayColor: const MaterialStatePropertyAll(overlayColor),
+    ),
+  ),
+  useMaterial3: true,
+);
+
+final appDarkTheme = ThemeData(
+  fontFamily: 'Poppins',
+  colorScheme: ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    primary: primaryColor,
+    seedColor: primaryColor,
+  ),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ButtonStyle(
+      shape: MaterialStatePropertyAll(
+        RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+      ),
+      foregroundColor: const MaterialStatePropertyAll(Colors.white),
+      backgroundColor: const MaterialStatePropertyAll(primaryColor),
+      overlayColor: const MaterialStatePropertyAll(overlayColor),
+    ),
+  ),
+  useMaterial3: true,
+);
--- lib/features/auth/presentation/presentation.dart
+++ lib/features/auth/presentation/presentation.dart
@@ -1,3 +1,4 @@
 export 'pages/pages.dart';
 export 'sign_in_bloc/sign_in_bloc.dart';
+export 'sign_up_bloc/sign_up_bloc.dart';
 export 'widgets/widgets.dart';
